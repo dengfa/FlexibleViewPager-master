e8fa61372b1cc727d5cde2dd5a46fae45fb16484
dengfa <dengfa@knowbox.cn>
2018-07-03 19:13:14 +0800

    fix

--- app/src/main/java/com/wcb/flexibleviewpager/view/FlexibleViewPager.java
+++ app/src/main/java/com/wcb/flexibleviewpager/view/FlexibleViewPager.java
@@ -1,13 +1,17 @@
 package com.wcb.flexibleviewpager.view;
 
 import android.content.Context;
+import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPropertyAnimatorListener;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.RelativeLayout;
+
+import com.wcb.flexibleviewpager.R;
 
 /**
  * Created by wcb on 2017/4/19.
@@ -18,6 +22,9 @@ public class FlexibleViewPager extends RelativeLayout {
     private OnRefreshListener listener;
     private ViewPager mViewPager;
     private float mDownX;
+    private View mLoadMoreLayout;
+    private int mRight;
+    private int mLoadMoreLayoutWidth;
 
     public FlexibleViewPager(Context context) {
         this(context, null);
@@ -33,18 +40,16 @@ public class FlexibleViewPager extends RelativeLayout {
     }
 
     @Override
-    protected void onAttachedToWindow() {
-        super.onAttachedToWindow();
-
-    }
-
-    @Override
-    protected void onFinishInflate() {
-        super.onFinishInflate();
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
         View view = getChildAt(0);
         if (view instanceof ViewPager) {
             mViewPager = (ViewPager) view;
         }
+        mLoadMoreLayout = getChildAt(1);
+        mRight = mViewPager.getRight();
+        mLoadMoreLayout.setTranslationX(mRight);
+        mLoadMoreLayoutWidth = getResources().getDimensionPixelSize(R.dimen.load_more_layout_width);
     }
 
     @Override
@@ -82,8 +87,10 @@ public class FlexibleViewPager extends RelativeLayout {
         void onLoadMore();
     }
 
+    private boolean isAutoScrolling;
 
     private class GestureListener implements GestureDetector.OnGestureListener {
+
 
         @Override
         public boolean onDown(MotionEvent e) {
@@ -102,13 +109,36 @@ public class FlexibleViewPager extends RelativeLayout {
 
         @Override
         public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+            //// TODO: 2018/7/3 这个distance为什么有时候不准？？？
+
+            if (e1 != null) {
+                Log.d("vincent", "onScroll e1 - " + e1.getRawX());
+            }
+
+            if (e2 != null) {
+                Log.d("vincent", "onScroll e2 - " + e2.getRawX());
+            }
+
             Log.d("vincent", "onScroll distanceX - " + distanceX);
             Log.d("vincent", "onScroll getTranslationX - " + mViewPager.getTranslationX());
+            if (isAutoScrolling) {
+                return true;
+            }
+
+            //加载更多中，右划
+            if (mViewPager.getTranslationX() < 0 && distanceX < 0) {
+                scrollToRight();
+            }
+
             if (mViewPager.getTranslationX() - distanceX > 0) {
                 distanceX = mViewPager.getTranslationX();
             }
-            if (Math.abs(distanceX) < 200) {
+            if (Math.abs(distanceX) < mLoadMoreLayoutWidth / 2) {
+                if (mViewPager.getTranslationX() - distanceX < -mLoadMoreLayoutWidth) {
+                    distanceX = mViewPager.getTranslationX() + mLoadMoreLayoutWidth;
+                }
                 mViewPager.setTranslationX(mViewPager.getTranslationX() - distanceX);
+                mLoadMoreLayout.setTranslationX(mLoadMoreLayout.getTranslationX() - distanceX);
             }
             return true;
         }
@@ -120,7 +150,58 @@ public class FlexibleViewPager extends RelativeLayout {
 
         @Override
         public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            Log.d("vincent", "onFling velocityX - " + velocityX);
+            Log.d("vincent", "onFling velocityY - " + velocityY);
+            if (velocityX < -2000 || mViewPager.getTranslationX() < -mLoadMoreLayoutWidth / 2) {
+                scrollToLeft();
+                return true;
+            } else if (velocityX > 2000 || mViewPager.getTranslationX() >= -mLoadMoreLayoutWidth / 2) {
+                scrollToRight();
+                return true;
+            }
             return false;
         }
+    }
+
+    private void scrollToRight() {
+        Log.d("vincent", "scrollToRight");
+        ViewCompat.animate(mViewPager).translationX(0).setListener(new ViewPropertyAnimatorListener() {
+            @Override
+            public void onAnimationStart(View view) {
+                isAutoScrolling = true;
+            }
+
+            @Override
+            public void onAnimationEnd(View view) {
+                isAutoScrolling = false;
+            }
+
+            @Override
+            public void onAnimationCancel(View view) {
+                isAutoScrolling = false;
+            }
+        });
+        ViewCompat.animate(mLoadMoreLayout).translationX(mRight);
+    }
+
+    private void scrollToLeft() {
+        Log.d("vincent", "scrollToLeft");
+        ViewCompat.animate(mViewPager).translationX(-mLoadMoreLayoutWidth).setListener(new ViewPropertyAnimatorListener() {
+            @Override
+            public void onAnimationStart(View view) {
+                isAutoScrolling = true;
+            }
+
+            @Override
+            public void onAnimationEnd(View view) {
+                isAutoScrolling = false;
+            }
+
+            @Override
+            public void onAnimationCancel(View view) {
+                isAutoScrolling = false;
+            }
+        });
+        ViewCompat.animate(mLoadMoreLayout).translationX(mRight - mLoadMoreLayoutWidth);
     }
 }
